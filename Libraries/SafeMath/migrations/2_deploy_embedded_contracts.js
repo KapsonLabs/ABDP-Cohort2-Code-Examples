3f34c114c83909a5387d4c293f22199a8ba0eeae
joshorig <cassidy.joshua@gmail.com>
2019-10-24 19:53:19 +0100

    update liraries example code

--- Libraries/SafeMath/migrations/2_deploy_embedded_contracts.js
+++ Libraries/SafeMath/migrations/2_deploy_embedded_contracts.js
@@ -1,5 +1,6 @@
 let SimpleBankEmbedded = artifacts.require("SimpleBankEmbedded");
 let SimpleBankEmbeddedUsing = artifacts.require("SimpleBankEmbeddedUsing");
+let SimpleBankZeppelin = artifacts.require("SimpleBankZeppelin");
 
 module.exports = function(deployer) {
     deployer.deploy(SimpleBankEmbedded);
